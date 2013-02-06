11a3f8dfaa8340e8a49f16a0d3ab2d62222138cd
glynnc67 <glynnc67@ea038faa-237f-11de-8bc4-bb72633a7edc>
2013-02-06 23:09:53 +0000

    Remove bogus dependency-installation scripts

--- makehuman/INSTALL_DEB_DEPENDENCIES.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo
-echo ON UBUNTU RUN "sudo ./INSTALL_DEB_DEPENDENCIES.bash"
-echo
-
-apt-get install python python-opengl python-numpy glew-utils python-dev python-imaging python-opengl python-qt4 python-qt4-gl python-svn
-
--- makehuman/INSTALL_RPM_DEPENDENCIES.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin
-
-yum install PyOpenGL numpy SDL SDL-devel SDL_image SDL_image-devel python-imaging libpng libjpeg libtiff
-
