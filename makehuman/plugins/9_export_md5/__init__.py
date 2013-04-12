45354e8b6d226563d6c8534ebdf6c85c1ccafbd1
thomas.larsson.01@gmail.com <thomas.larsson.01@gmail.com@ea038faa-237f-11de-8bc4-bb72633a7edc>
2013-04-12 12:03:13 +0000

    Moved some more exporters into plugins directory. Use cleanpyc.bat to avoid doubly defined plugins.

---
+++ makehuman/plugins/9_export_md5/__init__.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    http://code.google.com/p/makehuman/
+
+**Authors:**           Marc Flerackers, Thomas Larsson, Jonas Hauquier
+
+**Copyright(c):**      MakeHuman Team 2001-2013
+
+**Licensing:**         AGPL3 (see also http://www.makehuman.org/node/318)
+
+**Coding Standards:**  See http://www.makehuman.org/node/165
+
+Abstract
+--------
+
+TODO
+"""
+
+import gui
+from export import Exporter
+from exportutils.config import Config
+
+class MD5Config(Config):
+
+    def __init__(self, exporter):
+        Config.__init__(self)
+        self.selectedOptions(exporter)
+        self.useRelPaths = True
+        self.useTexFolder = exporter.useTexFolder
+    
+
+class ExporterMD5(Exporter):
+    def __init__(self):
+        Exporter.__init__(self)
+        self.name = "MD5"
+        self.filter = "MD5 (*.md5)"
+
+    def build(self, options, taskview):
+        Exporter.build(self, options, taskview)
+        self.hidden.hide()
+        # TODO remove scales option from this exporter
+
+    def export(self, human, filename):
+        from . import mh2md5
+        cfg = MD5Config(self)
+        cfg.selectedOptions(self)
+        mh2md5.exportMd5(human, filename("md5mesh"), cfg)
+
+def load(app):
+    app.addExporter(ExporterMD5())
+
+def unload(app):
+    pass
