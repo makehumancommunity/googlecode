6675cb4bae836f40a4a2f7cbfe3a00e5651ab6c7
Jonas Hauquier <jonas.hauquier@makehuman.org>
2013-04-27 13:35:20 +0000

    Use new filechooser for proxy library

--- makehuman/plugins/3_libraries_proxy_chooser.py
+++ makehuman/plugins/3_libraries_proxy_chooser.py
@@ -49,39 +49,28 @@ class ProxyAction(gui3d.Action):
 class ProxyFileSort(fc.FileSort):
     
     def __init__(self):
-        
         super(ProxyFileSort, self).__init__()
         self.meta = {}
     
     def fields(self):
-
         return list(super(ProxyFileSort, self).fields()) + ["faces"]
         
     def sortFaces(self, filenames):
-
         self.updateMeta(filenames)
         decorated = [(self.meta[filename]['faces'], i, filename) for i, filename in enumerate(filenames)]
         decorated.sort()
         return [filename for gender, i, filename in decorated]
         
     def updateMeta(self, filenames):
-        
         for filename in filenames:
-            
             if filename in self.meta:
-                
                 if self.meta[filename]['modified'] < os.path.getmtime(filename):
-                
                     self.meta[filename] = self.getMeta(filename)
-                
             else:
-                
                 self.meta[filename] = self.getMeta(filename)
                 
     def getMeta(self, filename):
-
         meta = {}
-                
         meta['modified'] = os.path.getmtime(filename)
         faces = 0
         try:
@@ -102,8 +91,10 @@ class ProxyTaskView(gui3d.TaskView):
     def __init__(self, category):
         
         gui3d.TaskView.__init__(self, category, 'Proxies')
-        self.filechooser = self.addTopWidget(fc.FileChooser('data/proxymeshes', 'proxy', 'thumb', 'data/proxymeshes/notfound.thumb', sort=ProxyFileSort()))
-        self.addLeftWidget(self.filechooser.sortBox)
+        #self.filechooser = self.addTopWidget(fc.FileChooser('data/proxymeshes', 'proxy', 'thumb', 'data/proxymeshes/notfound.thumb', sort=ProxyFileSort()))
+        self.filechooser = self.addRightWidget(fc.ListFileChooser('data/proxymeshes', 'proxy', 'Proxy', sort=ProxyFileSort()))
+        self.addLeftWidget(self.filechooser.createSortBox())
+        self.filechooser.refresh()
 
         @self.filechooser.mhEvent
         def onFileSelected(filename):
@@ -133,12 +124,10 @@ class ProxyTaskView(gui3d.TaskView):
 
     def onShow(self, event):
         # When the task gets shown, set the focus to the file chooser
-        gui3d.app.selectedHuman.hide()
         gui3d.TaskView.onShow(self, event)
         self.filechooser.setFocus()
 
     def onHide(self, event):
-        gui3d.app.selectedHuman.show()
         gui3d.TaskView.onHide(self, event)
 
     def onHumanChanging(self, event):
