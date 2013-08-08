650a587b1c02cd44e17e00b8681f01813b3ed6e5
joel.palmius@contuitus.com <joel.palmius@contuitus.com@ea038faa-237f-11de-8bc4-bb72633a7edc>
2013-08-08 07:44:09 +0000

    Exclude .svn dirs when rsyncing bin and debian dirs too

--- makehuman/deb/buildDeb.py
+++ makehuman/deb/buildDeb.py
@@ -15,8 +15,8 @@
 pre_deb_scripts = ["cleanpyc.sh","cleannpz.sh","compile_targets.py","compile_models.py"]
 
 rsync = "/usr/bin/rsync"
-rsync_common_args = "-av --delete"
-rsync_main_excludes = ["deb","SConstruct",".svn","*.pyc","*.nsi","*.pyd","*.c","*.h","*.target","standalone","compressTargetsASCII.py"]
+rsync_common_args = "-av --delete --exclude=.svn"
+rsync_main_excludes = ["deb","SConstruct","*.pyc","*.nsi","*.pyd","*.c","*.h","*.target","standalone","compressTargetsASCII.py"]
 
 svn = "/usr/bin/svn"
 
