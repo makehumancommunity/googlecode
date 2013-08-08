60037d7db2d4958b72b98d681e826fdde54eb68a
joel.palmius@contuitus.com <joel.palmius@contuitus.com@ea038faa-237f-11de-8bc4-bb72633a7edc>
2013-08-08 07:30:00 +0000

    Exclude compressTargetsASCII.py and tools/standalone from deb

--- makehuman/deb/buildDeb.py
+++ makehuman/deb/buildDeb.py
@@ -16,7 +16,7 @@ pre_deb_scripts = ["cleanpyc.sh","cleannpz.sh","compile_targets.py","compile_mod
 
 rsync = "/usr/bin/rsync"
 rsync_common_args = "-av --delete"
-rsync_main_excludes = ["deb","SConstruct",".svn","*.pyc","*.nsi","*.pyd","*.c","*.h","*.target"]
+rsync_main_excludes = ["deb","SConstruct",".svn","*.pyc","*.nsi","*.pyd","*.c","*.h","*.target","standalone","compressTargetsASCII.py"]
 
 svn = "/usr/bin/svn"
 
