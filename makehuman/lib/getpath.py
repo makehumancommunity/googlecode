7e90f9e84a417267d85019c4d7bd74958442f0c1
Jonas Hauquier <jonas.hauquier@makehuman.org>
2013-10-14 16:39:26 +0000

    Refactor getpath module

--- makehuman/lib/getpath.py
+++ makehuman/lib/getpath.py
@@ -25,14 +25,19 @@ Utility module for finding the user home path.
 import sys
 import os
 
-def getPath(type = None):
-    if isinstance(type, (str, unicode)):
-        typeStr = str(type)
-    elif type is None:
-        typeStr = ""
-    else:
-        raise TypeError("String expected")
+__home_path = None
 
+def getHomePath():
+    """
+    Find the user home path.
+    Note: If you are looking for MakeHuman data, you probably want getPath()!
+    """
+    # Cache the home path
+    global __home_path
+    if __home_path is not None:
+        return __home_path
+
+    # Windows
     if sys.platform == 'win32':
         import _winreg
         keyname = r'Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders'
@@ -40,64 +45,77 @@ def getPath(type = None):
         k = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, keyname)
         value, type_ = _winreg.QueryValueEx(k, 'Personal')
         if type_ == _winreg.REG_EXPAND_SZ:
-            path = _winreg.ExpandEnvironmentStrings(value)
+            __home_path = _winreg.ExpandEnvironmentStrings(value)
+            return __home_path
         elif type_ == _winreg.REG_SZ:
-            path = value
+            __home_path = value
+            return __home_path
         else:
             raise RuntimeError("Couldn't determine user folder")
 
-        if typeStr == "exports":
-            path += u"\\makehuman\\exports\\"
-        elif typeStr == "models":
-            path += u"\\makehuman\\models\\"
-        elif typeStr == "grab":
-            path += u"\\makehuman\\grab\\"
-        elif typeStr == "render":
-            path += u"\\makehuman\\render\\"
-        elif typeStr == "scenes":
-            path += u"\\makehuman\\scenes\\"
-        elif typeStr == "":
-            path += u"\\makehuman\\"
-        else:
-            raise ValueError("Unknown value '%s' for getPath()!" % typeStr)
+    # Unix-based
     else:
-        path = os.path.expanduser('~')
-        if sys.platform.startswith("darwin"): 
-            path = os.path.join(path,"Documents")
-            path = os.path.join(path,"MakeHuman")
-        else:
-            path = os.path.join(path,"makehuman")
+        __home_path = os.path.expanduser('~')
+        return __home_path
 
-        if typeStr == "exports":
-            path += "/exports/"
-        elif typeStr == "models":
-            path += "/models/"
-        elif typeStr == "grab":
-            path += "/grab/"
-        elif typeStr == "render":
-            path += "/render/"
-        elif typeStr == "scenes":
-            path += "/scenes/"
-        elif typeStr == "":
-            path += "/"
-        else:
-            raise ValueError("Unknown property '%s' for getPath()!" % typeStr)
+def getPath(type = None):
+    """
+    Get MakeHuman folder that contains per-user files, located in the user home
+    path.
+    """
+    if isinstance(type, (str, unicode)):
+        typeStr = str(type)
+    elif type is None:
+        typeStr = ""
+    else:
+        raise TypeError("String expected")
 
-    return path
+    path = getHomePath()
+
+    # Windows
+    if sys.platform == 'win32':
+        path = os.path.join(path, "makehuman")
+
+    # MAC OSX
+    elif sys.platform.startswith("darwin"):
+        path = os.path.join(path, "Documents")
+        path = os.path.join(path, "MakeHuman")
+
+    # Unix/Linux
+    else:
+        path = os.path.join(path, "makehuman")
+
+
+    if typeStr == "exports":
+        return os.path.join(path, 'exports')
+    elif typeStr == "models":
+        return os.path.join(path, 'models')
+    elif typeStr == "grab":
+        return os.path.join(path, 'grab')
+    elif typeStr == "render":
+        return os.path.join(path, 'render')
+    elif typeStr == "scenes":
+        return os.path.join(path, 'scenes')
+    elif typeStr == "":
+        return path
+    else:
+        raise ValueError("Unknown value '%s' for getPath()!" % typeStr)
 
 def getSysDataPath(subPath = ""):
     """
     Path to the data folder that is installed with MakeHuman system-wide.
     """
     if subPath:
-        return os.path.join("data/", subPath)
+        return getSysPath( os.path.join("data", subPath) )
     else:
-        return "data/"
+        return getSysPath("data/")
 
 def getSysPath(subPath):
     """
     Path to the system folder where MakeHuman is installed (it is possible that 
     data is stored in another path).
+    Writing to this folder or modifying this data usually requires admin rights,
+    contains system-wide data (for all users).
     """
     if subPath:
         return os.path.join(subPath)
