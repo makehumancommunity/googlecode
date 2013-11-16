Fixes for scaling and FOG for obj and stl export

makehuman/plugins/9_export_stl/mh2stl.py
@@ -75,9 +75,10 @@ def exportStlAscii(human, filepath, config, exportJoints = False):
 
     progress(0.3, 0.99, "Writing Objects")
     objprog = Progress(len(rmeshes))
+    offs = config.scale*config.offset
     for rmesh in rmeshes:
         obj = rmesh.object
-        coord = np.array([co-config.offset for co in obj.coord])
+        coord = np.array([co-offs for co in obj.coord])
         fp.write("".join( [(
             'facet normal %f %f %f\n' % tuple(obj.fnorm[fn]) +
             '\touter loop\n' +
@@ -133,11 +134,12 @@ def exportStlBinary(human, filepath, config, exportJoints = False):
 
     progress(0.3, 0.99, "Writing Objects")
     objprog = Progress(len(rmeshes))
+    offs = config.scale * config.offset
     for rmesh in rmeshes:
         obj = rmesh.object
         for fn,fv in enumerate(obj.fvert):
             fno = obj.fnorm[fn]
-            co = obj.coord[fv] - config.offset
+            co = obj.coord[fv] - offs
 
             fp.write(struct.pack('<fff', fno[0], fno[1], fno[2]))
             fp.write(struct.pack('<fff', co[0][0], co[0][1], co[0][2]))

makehuman/shared/wavefront.py
@@ -151,7 +151,7 @@ def writeObjFile(path, objects, writeMTL = True, config = None):
 
     # Vertices
     if config:
-        offs = config.offset
+        offs = config.scale * config.offset
     else:
         offs = np.array((0,0,0))
     for obj in objects: