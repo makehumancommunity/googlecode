Hopefully fixed global rotation in Collada export to Opensim.

makehuman/plugins/9_export_collada/dae_node.py
@@ -74,16 +74,15 @@ def writeSceneWithArmature(fp, rmeshes, amt, config):
 _Identity = np.identity(4, float)
 _RotX = tm.rotation_matrix(math.pi/2, (1,0,0))
 _RotZ = tm.rotation_matrix(math.pi/2, (0,0,1))
+_RotOpenSim = np.dot(_RotZ, _RotX)
 
 def globalMatrix(config):
     mat = np.identity(4, float)
     mat[:3,3] = -config.scale*config.offset
-    if config.rotate90X and config.rotate90Z:
-        rot = np.dot(_RotZ, _RotX)
+    if config.secondlife:
+        rot = _RotOpenSim
     elif config.rotate90X:
         rot = _RotX
-    elif config.rotate90Z:
-        rot = _RotZ
     else:
         rot = _Identity
     return np.dot(rot, mat)